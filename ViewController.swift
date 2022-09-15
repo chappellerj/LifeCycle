3395e78f94dcc86e1d689b9439cbea1b415afd5d
Ryan Chappelle <77121182+chappellerj@users.noreply.github.com>
2022-09-15 16:46:25 -0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  LifeCycle
+//
+//  Created by Ryan Chappelle on 9/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewController - View Did Load")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController - View Will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController - View Did Appear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("ViewController - View Will Disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("ViewController - View Did Disappear")
+    }
+}
+
